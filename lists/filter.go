32bd8235c6c0d789afcf3e80dd972e4f7c07eb8c
Tomderandere <tompbickle@gmail.com>
2025-02-22 16:51:49 +0100

    filterless

--- lists/filter.go
+++ lists/filter.go
@@ -9,9 +9,17 @@ func FilterLess(list []int, key int) []int {
 	// Wenn das erste Element größer als key ist, ist das Ergebnis die gefilterte Restliste.
 	// Wenn das erste Element kleiner oder gleich key ist, ist das Ergebnis das erste Element
 	// plus die gefilterte Restliste.
+	result := []int{}
+	if Empty(list) {
+		return []int{}
+	}
 
-	// TODO
-	return list
+	if list[0] <= key {
+		result = append(result, list[0])
+	}
+	rest := FilterLess(list[1:], key)
+	result = append(result, rest...)
+	return result
 }
 
 // Liefert eine Liste mit allen Elementen aus list, die echt größer als key sind.
