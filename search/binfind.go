032c93387044509bef49e855ceda614d3e7631fa
Tomderandere <tompbickle@gmail.com>
2025-02-24 16:14:56 +0100

    ich hasse die bin search

--- search/binfind.go
+++ search/binfind.go
@@ -4,6 +4,17 @@ package search
 // Falls x nicht gefunden wird, wird -1 zurückgegeben.
 // Da die Liste sortiert ist, wird die binäre Suche verwendet.
 func FindSorted(list []int, x int) int {
-	// TODO
-	return -1
+	if len(list) == 1 {
+		return -1
+	}
+	result := FindSorted(list[1:], x)
+	if list[0] == x {
+		return result
+	}
+	if result == -1 {
+		return -1
+	}
+
+	return result + 1
+
 }
